cc958a25178999d242ad945f0d0d501305512889
ahmethakanyldrm <hakan-336-ahmet@outlook.com>
2024-04-28 15:11:42 +0300

    repository design pattern started

---
+++ Blog.API/Repositories/Implementation/CategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Blog.API.Models.Domain;
+using Blog.API.Repositories.Interface;
+
+namespace Blog.API.Repositories.Implementation
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public Task<Category> CreateAsync(Category category)
+        {
+           
+        }
+    }
+}
