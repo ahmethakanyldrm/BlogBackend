76853b10362dbcc2be6c2e2b843d39e66a938408
ahmethakanyldrm <hakan-336-ahmet@outlook.com>
2024-04-28 15:10:34 +0300

    created controllers and added Actions.

---
+++ Blog.API/Controllers/CategoriesController.cs
@@ -0,0 +1,46 @@
+﻿using Blog.API.Data;
+using Blog.API.Models.Domain;
+using Blog.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.API.Controllers
+{
+    // https://localhost:xxxx/api/categories
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoriesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            // map dto to domain model
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await dbContext.AddAsync(category);
+
+            await dbContext.SaveChangesAsync();
+
+            // Domain Model to dto
+
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
+    }
+}
